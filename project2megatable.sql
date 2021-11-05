ff787942d58351e5df060bad1f5ea13d36d4713d
Reilly Koren <49700126+korenrh@users.noreply.github.com>
2021-11-05 14:54:15 -0500

    Updated SQL

--- project2megatable.sql
+++ project2megatable.sql
@@ -4,6 +4,8 @@ CREATE DATABASE parking_data;
 USE parking_data;
 
 # Creating mega_table
+# IMPORTANT: NOTE THAT YOU MUST ADJUST YOUR MYSQL TIMEOUT SETTINGS
+# THIS DATABASE TAKES ABOUT 3 MINUTES TO LOAD
 DROP TABLE IF EXISTS mega_table;
 CREATE TABLE mega_table (
 	summons_number BIGINT NOT NULL, #primary key that identifies all tickets
@@ -30,10 +32,10 @@ CREATE TABLE mega_table (
     violation_county VARCHAR(10),
     front_or_opposite VARCHAR(2),
     house_number VARCHAR(20), #cast to INT after replacing empties with 0
-    street_name VARCHAR(25),
+    street_name TEXT,
     intersecting_street VARCHAR(25), #MOSTLY NULL VALUES, could be dropped
-    date_first_observed INT(10), # could cast to date, mostly 0's though so could be dropped. Perhaps replace 0's with issue date?
-    law_section INT(4),
+    date_first_observed VARCHAR(20), # could cast to date, mostly 0's though so could be dropped. Perhaps replace 0's with issue date?
+    law_section VARCHAR(100),
     sub_division VARCHAR(5),
     violation_legal_code VARCHAR(10), #all zeroes, MUST DROP
     days_parking_in_effect VARCHAR(10), # likely dependent on street
@@ -41,11 +43,11 @@ CREATE TABLE mega_table (
     to_hours_in_effect VARCHAR(10), #Cast to time, replace NULL with midnight?
     vehicle_color VARCHAR(10),
     unregistered_vehicle VARCHAR(10), #empty = 0, 0 = registered, 1 =unregistered
-    vehicle_year INT(4), #25% are 0 for this, maybe cast to NULL
+    vehicle_year VARCHAR(10), #25% are 0 for this, maybe cast to NULL
     meter_number VARCHAR(100), # MOSTLY NULL, we can drop this
-    feet_from_curb INT(10), # almost all 0's, could also drop
-    violation_post_code VARCHAR(10), # could cast to INT after replacing empties with 0
-    violation_description VARCHAR(50), # replace NULL with 'unspecified' or fill using rule
+    feet_from_curb VARCHAR(10), # almost all 0's, could also drop
+    violation_post_code TEXT, # could cast to INT after replacing empties with 0
+    violation_description VARCHAR(100), # replace NULL with 'unspecified' or fill using rule
     no_standing_or_stopping VARCHAR(10), # ALL NULL, DROP
     hydrant_violation VARCHAR(10), # ALL NULL, DROP
     double_parking VARCHAR(10), # ALL NULL, DROP
@@ -63,7 +65,11 @@ PRIMARY KEY (summons_number));
 # so remember to only download and load that one in!
 
 LOAD DATA INFILE '/Users/reillykoren/Desktop/Parking_Violations_Issued_-_Fiscal_Year_2014__August_2013___June_2014_.csv'
-INTO TABLE mega_table
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
+ INTO TABLE mega_table
+ FIELDS TERMINATED BY ',' 
+ ENCLOSED BY '"'
+ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
+
+SELECT * FROM mega_table
+LIMIT 100;
